e066adda24082bb773b211e5dfaa5bfd01bfcec1
Gerome Egron <legunda.gg&gmail.com>
2015-03-06 19:43:50 +0100

    First grid

--- src/main/java/fr/xebia/google/hashcode/structure/Grid.java
+++ src/main/java/fr/xebia/google/hashcode/structure/Grid.java
@@ -1,4 +1,21 @@
 package fr.xebia.google.hashcode.structure;
 
 public class Grid {
+
+    private final int lineSize;
+    private final int columnSize;
+
+    private Cell[] cells;
+
+    public Grid(int lineSize, int columnSize) {
+        this.lineSize = lineSize;
+        this.columnSize = columnSize;
+
+        cells = new Cell[lineSize * columnSize];
+    }
+
+    public Cell getCell(int lineNumber, int columnNumber) {
+        return cells[lineNumber * columnSize + columnNumber];
+    }
+
 }
---
+++ src/test/java/fr/xebia/google/hashcode/structure/GridTest.java
@@ -0,0 +1,17 @@
+package fr.xebia.google.hashcode.structure;
+
+import org.junit.Test;
+
+public class GridTest {
+
+    @Test
+    public void should_return_cell() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+
+}
